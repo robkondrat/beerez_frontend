9eb43bad31fc9b79eba358a75fc3c8bda2de409b
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-09-30 12:36:47 -0500

    capturing selected quantity on beers show page

--- src/views/BeersShow.vue
+++ src/views/BeersShow.vue
@@ -2,11 +2,21 @@
   <div class="beer-show">
     <h1>{{ beer.name }}</h1>
     <p>${{ beer.price }}</p>
+
+    <label for="quantity">Quantity:</label>
+    <input 
+      type="number" 
+      id="quantity" 
+      name="quantity" 
+      min="1"
+      v-on:change="setQuantity($event)"
+      >
+    <button type="button" class="btn btn-primary" v-on:click="getQuantity($event)">Add to Cart</button>
+
     <p>{{ beer.category }}</p>
     <img :src="beer.image_url" :alt="beer.name" width=300>
     <p>{{ beer.description }}</p>
     <p>ABV: {{ beer.abv }}%</p>
-    <p>
       Brewery: 
       <div>
         <router-link :to="'/breweries/' + beer.brewery.id">
@@ -18,7 +28,6 @@
           <img :src="beer.brewery.image_url" :alt="beer.brewery.name">
         </router-link>
       </div>
-    </p>
   </div>
 </template>
 
@@ -46,7 +55,9 @@
             name: "",
             image_url: ""
           }
-        }
+        },
+        formCompleted: false,
+        selectedQuantity: ""
       };
     },
     created() {
@@ -54,7 +65,16 @@
         .then(response => {
           console.log(response.data);
           this.beer = response.data;
-        })
+        });
+    },
+    methods: {
+      setQuantity(event) {
+        this.selectedQuantity = event.target.value
+        console.log(this.selectedQuantity)
+      },
+      getQuantity(event) {
+        // console.log(event.target)
+      }
     }
   }
 </script>
